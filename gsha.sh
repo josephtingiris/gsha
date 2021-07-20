05f87809879415d3479cfeb9c5ca4c723c8d2758
Joseph Tingiris <joseph.tingiris@gmail.com>
2021-07-20 08:37:54 -0400

    modulo 32; no env

--- gsha.sh
+++ gsha.sh
@@ -4,10 +4,9 @@
 
 cd "$(dirname $0)"
 
-let LOOP=$((((RANDOM<<15)|RANDOM) % 8))
+let LOOP=$((((RANDOM<<15)|RANDOM) % 32))
 
 if [ ${LOOP} -gt 0 ]; then
-    env
     for ((L=0; L<$LOOP; L++)); do
         ./gsha
         git add data 1> /dev/null
